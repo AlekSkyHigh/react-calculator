fe4ffbe9a3d09448573837d105cb195206387ca5
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-09-26 16:51:14 +0300

    onClick events added

--- src/App.js
+++ src/App.js
@@ -2,34 +2,42 @@ import './App.css';
 
 function Calculator() {
 
+  function handleNumber(){
+    alert('handleNumber click');
+  }
+
+  function handleOperator(){
+    alert('handleOperator click');
+  }
+
   return (
       <div className='calculator'>
         <div className='display'>0</div>
-          <CalcButton value="7"/>
-          <CalcButton value="8"/>
-          <CalcButton value="9"/>
-          <CalcButton className="operator" value="/"/>
+          <CalcButton value="7" onClick={handleNumber}/>
+          <CalcButton value="8" onClick={handleNumber}/>
+          <CalcButton value="9" onClick={handleNumber}/>
+          <CalcButton className="operator" value="/" onClick={handleOperator}/>
 
-          <CalcButton value="4"/>
-          <CalcButton value="5"/>
-          <CalcButton value="6"/>
-          <CalcButton className="operator" value="*"/>
+          <CalcButton value="4" onClick={handleNumber}/>
+          <CalcButton value="5" onClick={handleNumber}/>
+          <CalcButton value="6" onClick={handleNumber}/>
+          <CalcButton className="operator" value="*" onClick={handleOperator}/>
 
-          <CalcButton value="1"/>
-          <CalcButton value="2"/>
-          <CalcButton value="3"/>
-          <CalcButton className="operator" value="-"/>
+          <CalcButton value="1" onClick={handleNumber}/>
+          <CalcButton value="2" onClick={handleNumber}/>
+          <CalcButton value="3" onClick={handleNumber}/>
+          <CalcButton className="operator" value="-" onClick={handleOperator}/>
 
           <CalcButton value="C"/>
-          <CalcButton value="0"/>
+          <CalcButton value="0" onClick={handleNumber}/>
           <CalcButton value="="/>
-          <CalcButton className="operator" value="+"/>
+          <CalcButton className="operator" value="+" onClick={handleOperator}/>
       </div>
   )
 }
 
 function CalcButton(props){
-  return <button className={props.className}>{props.value}</button>
+  return <button className={props.className} onClick={props.onClick}>{props.value}</button>
 }
 
 export default Calculator;
